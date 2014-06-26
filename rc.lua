4ae0eab57841e09e36c17521560a4942eaf06e6f
Nicholas Chittle <nichittle@gmail.com>
2014-06-26 16:15:24 -0700

    Alias Commit

--- rc.lua
+++ rc.lua
@@ -236,10 +236,6 @@ globalkeys = awful.util.table.join(
 
     awful.key({ modkey,           }, "s",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "s",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "s",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
 
