20c22eab5d957f5b701f0aee5efb9c6b88e852eb
Nicholas Chittle <nichittle@gmail.com>
2014-06-29 15:16:30 -0700

    Alias Commit

--- rc.lua
+++ rc.lua
@@ -111,8 +111,7 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 
 -- {{{ Wibox
 -- Create a textclock widget
-mytextclock = awful.widget.textclock()
-
+mytextclock = awful.widget.textclock("%a %d %b %I:%M %p ", 60)
 -- Keyboard map indicator and changer
 kbdcfg = {}
 kbdcfg.cmd = "setxkbmap"
